8fb2c52abf51ebeefa39a132a33f269e613ad70f
Advait Thakar <advaitthakar@Advaits-MacBook-Air.local>
2025-02-01 11:49:10 +0530

    frontend boilerplate

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <>
+      <div className='flex min-h-screen flex-col items-center justify-center'>
+        <h1>Hello HealthMate!</h1>
+        <Button>Heyy there!</Button>
+      </div>
+    </>
+  )
+}
+
+export default App

d552fa0947d60be9bcc54fbc67517db6ca562c99
LimbachiyaKrina <limbachiyakrina157@gmail.com>
2025-02-01 22:16:06 +0530

    form-added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import Appointment from "../src/pages/Appointment/Appointment";
 import DoctorBooking from "../src/pages/Booking/Booking";
 import Home from "../src/pages/Home/Home";
 import Profile from "../src/pages/Profile/Profile";
-
+import ProfileForm from "../src/pages/ProfileForm/ProfileForm"
 
 function App() {
   return (
@@ -16,6 +16,7 @@ function App() {
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/appointment" element={<Appointment />} />
           <Route path="/book-appointment/:doctorId" element={<DoctorBooking />} />
+          <Route path="/profileform" element={<ProfileForm/>} />
       </Routes>
     </Router>  
     </div>

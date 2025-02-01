98a9bd5a11b3c859b8d715577d75a53541ee2e0c
LimbachiyaKrina <limbachiyakrina157@gmail.com>
2025-02-01 23:37:42 +0530

    Healthcare-1

---
+++ frontend/src/pages/HealthCare/HealthCare.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Layout from "../../Layout/Layout";
+import HealthCare from "../../Components/ui/HealthCare/HealthCare";
+
+const HealthCare = () => {
+  return (
+    <Layout showFooter={true}>
+      <HealthCare></HealthCare>
+    </Layout>
+  );
+};
+
+export default HealthCare;

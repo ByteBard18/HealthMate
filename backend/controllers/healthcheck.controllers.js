9bd7481085a6049575416a2fd94001ec1b11ce72
Advait Thakar <advaitthakar@Advaits-MacBook-Air.local>
2025-02-01 13:07:17 +0530

    .env change

--- backend/controllers/healthcheck.controllers.js
+++ backend/controllers/healthcheck.controllers.js
@@ -4,7 +4,7 @@ import { AsyncHandler } from "../utils/AsyncHandler.js"
 
 const healthcheck = AsyncHandler(async (req, res) => {
     try {
-        return res.status(200).json(new ApiResponse(200, {}, "Healthcheck successful"))
+        return res.status(200).json(new ApiResponse(200, {}, "Healthcheck successfully finished!"))
     } catch (error) {
         return res.status(400).json(new ApiError(400, error.message))
     }

43a79c2e8ae6c57032885e054217323b2a72a50e
LimbachiyaKrina <limbachiyakrina157@gmail.com>
2025-02-01 12:38:52 +0530

    cleck-login-setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,21 @@
 import './App.css'
 import { Button } from './components/ui/button'
 
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react'
+
+
 function App() {
 
   return (
     <>
+     <header>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </header>
       <div className='flex min-h-screen flex-col items-center justify-center'>
         <h1>Hello HealthMate!</h1>
         <Button>Heyy there!</Button>
@@ -14,3 +25,4 @@ function App() {
 }
 
 export default App
+

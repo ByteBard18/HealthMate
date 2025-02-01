315d9e2fbcebae651f314bac67ec90873749e0fd
Advait Thakar <advaitthakar@Advaits-MacBook-Air.local>
2025-02-01 12:52:37 +0530

    backend boilerplate with healthcheck routes

---
+++ backend/utils/ApiError.js
@@ -0,0 +1,19 @@
+class ApiError extends Error {
+    constructor(statusCode, message="Something went wrong!", errors=[], stackTrace="") {
+        super(message)
+        this.statusCode = statusCode
+        this.errors = errors
+        this.message = message
+        this.data = null
+        this.success = false
+
+        if(stackTrace) {
+            this.stackTrace = stackTrace
+        }
+        else {
+            Error.captureStackTrace(this, this.constructor)
+        }
+    }
+}
+
+export { ApiError }
